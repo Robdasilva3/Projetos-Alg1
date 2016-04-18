6db03fa0a3b545be41c7d30e41c7b5a4a1177867
Robzod <robertorachadel@hotmail.com>
2016-04-18 15:32:42 -0300

    Teste de If e Else

---
+++ Ifelse.java
@@ -0,0 +1,11 @@
+public class Ifelse {
+    public static void main(String[] args) {
+        if (1 == 2){
+            System.out.println("Você nunca lerá essa mensagem, mwahuahuauha");
+        }
+       
+        if (1 == 1){
+            System.out.println("1 é igual a 1? Jura? ");
+        }
+    }
+}
