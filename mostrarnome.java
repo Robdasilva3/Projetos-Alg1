a6f8779fdd6f9857ba0538b5a0dde138aa1bdaac
Robzod <robertorachadel@hotmail.com>
2016-04-18 15:36:42 -0300

    Mostrar Nome

---
+++ mostrarnome.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+	public class mostrarnome {
+
+		public static void main(String[] args) {
+
+			String nome;
+			nome = "Roberto";
+
+					JOptionPane.showMessageDialog(null,"Meu Nome é: " + nome +"!");
+
+		}
+
+	}
