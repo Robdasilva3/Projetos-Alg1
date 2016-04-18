81f60cd3c56756103b5894aba9ac4d3a4ea4da08
Robzod <robertorachadel@hotmail.com>
2016-04-18 15:37:26 -0300

    Mostrar Número

---
+++ mostrarnumero.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+	public class mostrarnumero {
+
+		public static void main(String[] args) {
+
+			int numero;
+			numero = 18;
+
+				JOptionPane.showMessageDialog(null,"Sua idade é: " + numero );
+
+		}
+
+	}
